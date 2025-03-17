ddf28615fd752bfdd3c6c6ca6321e352a0ef54d1
Syed Shuaib <syedshuaib@Syeds-MacBook-Air.local>
2025-03-17 14:27:43 +0530

    Final Push

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -23,6 +23,6 @@ router.register(r"tasks", TaskViewSet, basename="task")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include(router.urls)),  # Added versioning to API URL
-    path("", lambda request: redirect("/api/v1/tasks/")),  # Redirect `/` to `/api/v1/tasks/`
+    path("api/v1/", include(router.urls)),  
+    path("", lambda request: redirect("/api/v1/tasks/")), 
 ]
